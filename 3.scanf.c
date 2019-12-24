c90d321a0bc16a649ec4703374d02eef4408edd0
Woo-Vin Choi <42290980+cwvv@users.noreply.github.com>
2019-12-24 09:54:08 +0900

    Update scanf

--- 3.scanf.c
+++ 3.scanf.c
@@ -2,9 +2,16 @@
 
 int main(void)
 {
-  int input;
+  int a, b, result;
+
   printf("정수를 입력하세요\n");
-  scanf("%d", &input);
-  printf("당신이 입력한 값은 %d입니다.\n", input);
+  scanf("%d", &a);
+
+  printf("정수를 입력하세요\n");
+  scanf("%d", &b);
+
+  result = a + b;
+  printf("당신이 입력한 총합은 %d입니다.\n", result);
+
   return 0;
 }
