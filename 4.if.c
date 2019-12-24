639ba941378a7f8a681c536a687d3681bbe5f4e1
Woo-Vin Choi <42290980+cwvv@users.noreply.github.com>
2019-12-24 10:05:16 +0900

    Add if

---
+++ 4.if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int password;
+
+  printf("비밀번호를 입력하세요.\n");
+  scanf("%d", &password);
+
+  if (password == 1234) {
+    printf("딩동댕.\n");
+  } else {
+    printf("땡.\n");
+
+  }
+  return 0;
+}
