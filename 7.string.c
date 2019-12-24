49db61d7d9ba2d0a6e79926dc3e2b9f7f2c1618d
Woo-Vin Choi <42290980+cwvv@users.noreply.github.com>
2019-12-24 13:58:54 +0900

    Add string

--- 7.string.c
+++ 7.string.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-  char string[] = "Hi";
-  puts(string);
+  char string[10] = "Hi";
+  char str2[10];
+  char str3[] = "Hia";
+  char *str4;
+  char str5[100] = "https://google.co.uk"
+  char *str6;
+  char str7[10] = "google"
+
+  int length = strlen(string);
+  printf("strlen: %d\n", length);
+
+  strcpy(str2, string);
+  puts(str2);
+
+  printf("string과 str2의 비교 : %d\n", strcmp(string, str2));
+  printf("string과 str3의 비교 : %d\n", strcmp(str3, string));
+
+  printf("문자열 추가 : %s\n", strcat(string, "i"));
+
+  str4 = strchr(string, 'i');
+  printf("%s\n", str4);
+
+  str6 = strstr(str5, str7)
+  printf("%s\n", str6);
   return 0;
 }
