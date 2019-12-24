eeb24730d088ada18b6131f4abe9b7409a12db13
Woo-Vin Choi <42290980+cwvv@users.noreply.github.com>
2019-12-24 10:16:53 +0900

    Add while

---
+++ 5.while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int input,n;
+
+  printf("자연수를 입력하세요\n");
+  scanf("%d", &input);
+
+  n = 0;
+  while (input+1 > n) {
+    printf("%d\n", n);
+    n++;
+  }
+
+  return 0;
+}
