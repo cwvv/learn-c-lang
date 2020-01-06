1474392b625892cbc5f9028456145ae3bbefe313
Woo-Vin Choi <42290980+cwvv@users.noreply.github.com>
2020-01-06 21:38:20 +0900

    invert_string machine

--- C_pratice/C_pratice/my rhkwp2.c
+++ C_pratice/C_pratice/my rhkwp2.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-void invert_string(char x[]) {
-	int i;
-
-	i = strlen(x) - 1;
-
-	while ( i >= 0 ) {
-		printf("%c", x[i]);
-		i--;
-	}
-}
-
 int main(void) {
-	char str[200];
-	char str2[200] = { '\0' };
+	char str1[200];
+	char str2[200];
 	
 	printf("in word\n");
-	scanf("%s", str);
+	scanf("%s", str1);
 
-	strcpy(str2, invert_string(str));
+	strcpy(str2, str1);
 
-	if (strcmp(str, str2) == 0) {
+	int i, n, m;
+
+	i = strlen(str1);
+	m = strlen(str1) - 1;
+
+	n = 0;
+	while (n < i) {
+		str2[n] = str2[m - n];
+		n++;
+	}
+
+	if (strcmp(str1, str2) == 0) {
 		printf("same");
 	} else {
 		printf("different");
